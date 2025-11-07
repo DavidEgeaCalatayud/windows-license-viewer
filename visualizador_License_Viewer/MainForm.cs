18364f0bfd7d0e48ec9225d30d7bb29a07b27465
DavidEgeaCalatayud <davidegeacalatayud@gmail.com>
2025-11-07 15:40:01 +0100

    Editable

--- visualizador_License_Viewer/MainForm.cs
+++ visualizador_License_Viewer/MainForm.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Drawing;
-using System.IO;
 using System.Management;
-using System.Text;
 using System.Windows.Forms;
 using Microsoft.Win32;
 
@@ -11,8 +9,10 @@ namespace visualizador_License_Viewer
     public class MainForm : Form
     {
         PictureBox logo;
-        Label lblProductKey;
-        Label lblSerial;
+        Label lblProductKeyTitle;
+        Label lblSerialTitle;
+        TextBox txtProductKey;
+        TextBox txtSerial;
 
         public MainForm()
         {
@@ -24,57 +24,134 @@ namespace visualizador_License_Viewer
         {
             this.Text = "Información del sistema";
             this.StartPosition = FormStartPosition.CenterScreen;
-            this.Width = 500;
-            this.Height = 350;
+            this.ClientSize = new Size(470, 320);
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Font = new Font("Segoe UI", 10);
 
-            // Logo Windows
             logo = new PictureBox()
-
             {
                 Image = Properties.Resources.logoWindows,
                 SizeMode = PictureBoxSizeMode.Zoom,
-                Left = 100,
-                Top = 20,
                 Width = 250,
                 Height = 120,
             };
+
+            txtProductKey = new TextBox()
+            {
+                Width = 460,
+                Height = 28,
+                Font = new Font("Consolas", 11, FontStyle.Regular),
+                BorderStyle = BorderStyle.FixedSingle,
+                BackColor = Color.White,
+                TextAlign = HorizontalAlignment.Center
+            };
+            txtProductKey.KeyDown += TxtField_KeyDown;
+
+            lblProductKeyTitle = new Label()
+            {
+                Text = "Clave de producto de Windows:",
+                AutoSize = true,
+                Font = new Font("Segoe UI", 11, FontStyle.Regular)
+            };
+
+            txtSerial = new TextBox()
+            {
+                Width = 460,
+                Height = 28,
+                Font = new Font("Consolas", 11, FontStyle.Regular),
+                BorderStyle = BorderStyle.FixedSingle,
+                BackColor = Color.White,
+                TextAlign = HorizontalAlignment.Center
+            };
+            txtSerial.KeyDown += TxtField_KeyDown;
+
+            // Etiqueta Número de serie
+            lblSerialTitle = new Label()
+            {
+                Text = "Número de serie:",
+                AutoSize = true,
+                Font = new Font("Segoe UI", 11, FontStyle.Regular)
+            };
+
+            CenterElements();
+
+            // Agregar controles al formulario
             this.Controls.Add(logo);
+            this.Controls.Add(lblProductKeyTitle);
+            this.Controls.Add(txtProductKey);
+            this.Controls.Add(lblSerialTitle);
+            this.Controls.Add(txtSerial);
 
-            // Clave de producto
-            lblProductKey = new Label()
-            {
-                Text = "Clave de producto de Windows: ",
-                AutoSize = true,
-                Font = new Font("Segoe UI", 11, FontStyle.Regular),
-                Top = 160,
-                Left = 10
-            };
-
-            // Número de serie
-            lblSerial = new Label()
-            {
-                Text = "Número de serie: ",
-                AutoSize = true,
-                Font = new Font("Segoe UI", 11, FontStyle.Regular),
-                Top = 200,
-                Left = 10
-            };
-
-            this.Controls.Add(lblProductKey);
-            this.Controls.Add(lblSerial);
+            // Ajustar posiciones al redimensionar
+            this.Resize += (s, e) => CenterElements();
         }
 
+        private void CenterElements()
+        {
+            int formWidth = this.ClientSize.Width;
+            int formHeight = this.ClientSize.Height;
+
+            logo.Left = (formWidth - logo.Width) / 2;
+            logo.Top = (formHeight - 350) / 2 + 10;
+
+            // Posición relativa debajo del logo
+            int spacing = 20;
+            int topStart = logo.Bottom + spacing;
+
+            lblProductKeyTitle.Top = topStart;
+            lblProductKeyTitle.Left = (formWidth - lblProductKeyTitle.PreferredWidth) / 2;
+
+            txtProductKey.Top = lblProductKeyTitle.Bottom + 5;
+            txtProductKey.Left = (formWidth - txtProductKey.Width) / 2;
+
+            lblSerialTitle.Top = txtProductKey.Bottom + 20;
+            lblSerialTitle.Left = (formWidth - lblSerialTitle.PreferredWidth) / 2;
+
+            txtSerial.Top = lblSerialTitle.Bottom + 5;
+            txtSerial.Left = (formWidth - txtSerial.Width) / 2;
+        }
+
+        private void TxtField_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                var textBox = sender as TextBox;
+                if (textBox != null)
+                {
+                    if (textBox.ReadOnly)
+                    {
+                        textBox.ReadOnly = false;
+                        textBox.BorderStyle = BorderStyle.FixedSingle;
+                        textBox.BackColor = Color.White;
+                        textBox.ForeColor = Color.Black;
+                        textBox.Cursor = Cursors.IBeam;
+                        textBox.Focus();
+                        textBox.SelectionStart = textBox.Text.Length;
+                    }
+                    else
+                    {
+                        textBox.ReadOnly = true;
+                        textBox.BorderStyle = BorderStyle.None;
+                        textBox.BackColor = this.BackColor;
+                        textBox.ForeColor = Color.Black;
+                        textBox.Cursor = Cursors.Default;
+                        this.ActiveControl = null;
+                    }
+                }
+
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
 
         void LoadInfo()
         {
             string productKey = GetWindowsProductKey() ?? "(No disponible / requiere permisos)";
             string serial = GetSystemSerial() ?? "(No disponible)";
 
-            lblProductKey.Text = "Clave de producto de Windows: " + productKey;
-            lblSerial.Text = "Número de serie: " + serial;
+            txtProductKey.Text = productKey;
+            txtSerial.Text = serial;
         }
 
         string GetSystemSerial()
